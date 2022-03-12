b7b018fd568623c26695148dba015ea9c50d3037
Klemen Tušar <techouse@gmail.com>
2022-03-12 13:35:24 +0000

    :zap: Add basic file caching

--- bin/main.dart
+++ bin/main.dart
@@ -1,14 +1,112 @@
-import 'dart:io' show exitCode, stdout;
+import 'dart:io' show Platform, exitCode, stdout;
 
 import 'package:alfred_workflow/alfred_workflow.dart'
-    show AlfredItem, AlfredItemIcon, AlfredItemText, AlfredWorkflow;
+    show
+        AlfredItem,
+        AlfredItemIcon,
+        AlfredItemText,
+        AlfredItems,
+        AlfredWorkflow;
 import 'package:algolia/algolia.dart' show AlgoliaQuerySnapshot;
 import 'package:args/args.dart' show ArgParser, ArgResults;
-import 'package:html_unescape/html_unescape.dart' show HtmlUnescape;
+import 'package:html_unescape/html_unescape.dart';
+import 'package:path/path.dart' show dirname;
+import 'package:stash/stash_api.dart'
+    show
+        Cache,
+        CacheEntryCreatedEvent,
+        CacheEntryEvictedEvent,
+        CacheEntryExpiredEvent,
+        CacheEntryRemovedEvent,
+        CacheEntryUpdatedEvent,
+        CreatedExpiryPolicy,
+        EventListenerMode,
+        LruEvictionPolicy,
+        CacheExtension;
+import 'package:stash_file/stash_file.dart'
+    show FileCacheStore, newFileLocalCacheStore;
 
-import 'src/extensions/truncate.dart' show Truncate;
+import 'src/extensions/string_helpers.dart' show StringHelpers;
 import 'src/models/search_result.dart' show SearchResult;
 import 'src/services/algolia_search.dart' show AlgoliaSearch;
+
+final HtmlUnescape unescape = HtmlUnescape();
+
+final AlfredWorkflow workflow = AlfredWorkflow();
+
+final FileCacheStore store = newFileLocalCacheStore(
+  path: dirname(Platform.script.toFilePath()),
+  fromEncodable: (Map<String, dynamic> json) => AlfredItems.fromJson(json),
+);
+
+final Cache cache = store.cache<AlfredItems>(
+  name: 'query_cache',
+  maxEntries: 10,
+  eventListenerMode: EventListenerMode.synchronous,
+  evictionPolicy: const LruEvictionPolicy(),
+  expiryPolicy: const CreatedExpiryPolicy(Duration(minutes: 1)),
+);
+
+bool verbose = false;
+
+void main(List<String> arguments) async {
+  try {
+    exitCode = 0;
+
+    workflow.clearItems();
+
+    final ArgParser parser = ArgParser()
+      ..addOption('query', abbr: 'q', mandatory: true)
+      ..addFlag('verbose', abbr: 'v', defaultsTo: false);
+    final ArgResults args = parser.parse(arguments);
+
+    verbose = args['verbose'];
+
+    final String queryString =
+        args['query'].replaceAll(RegExp(r'\s+'), ' ').trim();
+
+    if (verbose) {
+      stdout.writeln('Query: "$queryString"');
+      _cacheVerbosity();
+    }
+
+    if (queryString.isEmpty) {
+      _showPlaceholder();
+    } else {
+      final AlfredItems? cachedItem = await cache.get(queryString.md5hex);
+      if (cachedItem != null) {
+        workflow.addItems(cachedItem.items);
+      } else {
+        await _performSearch(queryString);
+      }
+    }
+  } on FormatException catch (err) {
+    exitCode = 2;
+    workflow.addItem(AlfredItem(title: err.toString()));
+  } catch (err) {
+    exitCode = 1;
+    workflow.addItem(AlfredItem(title: err.toString()));
+    if (verbose) {
+      rethrow;
+    }
+  } finally {
+    workflow.run();
+  }
+}
+
+void _cacheVerbosity() {
+  cache
+    ..on<CacheEntryCreatedEvent<AlfredItems>>()
+        .listen((event) => print('Key "${event.entry.key}" added'))
+    ..on<CacheEntryUpdatedEvent<AlfredItems>>()
+        .listen((event) => print('Key "${event.newEntry.key}" updated'))
+    ..on<CacheEntryRemovedEvent<AlfredItems>>()
+        .listen((event) => print('Key "${event.entry.key}" removed'))
+    ..on<CacheEntryExpiredEvent<AlfredItems>>()
+        .listen((event) => print('Key "${event.entry.key}" expired'))
+    ..on<CacheEntryEvictedEvent<AlfredItems>>()
+        .listen((event) => print('Key "${event.entry.key}" evicted'));
+}
 
 void _showPlaceholder() {
   workflow.addItem(
@@ -23,10 +121,10 @@ Future<void> _performSearch(String query) async {
   final AlgoliaQuerySnapshot snapshot = await AlgoliaSearch.query(query);
 
   if (snapshot.nbHits > 0) {
-    final HtmlUnescape unescape = HtmlUnescape();
-
-    workflow.addItems(
-      snapshot.hits.map((snapshot) => SearchResult.fromJson(snapshot.data)).map(
+    final AlfredItems items = AlfredItems(
+      items: snapshot.hits
+          .map((snapshot) => SearchResult.fromJson(snapshot.data))
+          .map(
         (result) {
           final int level = int.tryParse(result.type.substring(3)) ?? 0;
           final String? title = result.hierarchy.getLevel(level);
@@ -38,7 +136,7 @@ Future<void> _performSearch(String query) async {
             title: title!,
             subtitle: level > 0
                 ? unescape.convert(hierarchy.values.join(' > ')).truncate(75)
-                : null,
+                : '',
             arg: result.url,
             text: AlfredItemText(
               largeType: title,
@@ -49,8 +147,10 @@ Future<void> _performSearch(String query) async {
             valid: true,
           );
         },
-      ),
+      ).toList(),
     );
+    cache.putIfAbsent(query.md5hex, items);
+    workflow.addItems(items.items);
   } else {
     final Uri url =
         Uri.https('www.google.com', '/search', {'q': 'React $query'});
@@ -68,45 +168,5 @@ Future<void> _performSearch(String query) async {
         valid: true,
       ),
     );
-  }
-}
-
-final AlfredWorkflow workflow = AlfredWorkflow();
-bool verbose = false;
-
-void main(List<String> arguments) async {
-  try {
-    exitCode = 0;
-
-    workflow.clearItems();
-
-    final ArgParser parser = ArgParser()
-      ..addOption('query', abbr: 'q', mandatory: true)
-      ..addFlag('verbose', abbr: 'v', defaultsTo: false);
-    final ArgResults args = parser.parse(arguments);
-
-    final String queryString =
-        args['query'].replaceAll(RegExp(r'\s+'), ' ').trim();
-
-    if (args['verbose']) verbose = true;
-
-    if (verbose) stdout.writeln('Query: "$queryString"');
-
-    if (queryString.isEmpty) {
-      _showPlaceholder();
-    } else {
-      await _performSearch(queryString);
-    }
-  } on FormatException catch (err) {
-    exitCode = 2;
-    workflow.addItem(AlfredItem(title: err.toString()));
-  } catch (err) {
-    exitCode = 1;
-    workflow.addItem(AlfredItem(title: err.toString()));
-    if (verbose) {
-      rethrow;
-    }
-  } finally {
-    workflow.run();
   }
 }
--- bin/src/extensions/string_helpers.dart
+++ bin/src/extensions/string_helpers.dart
@@ -1,4 +1,10 @@
-extension Truncate on String {
+import 'dart:convert' show utf8;
+
+import 'package:crypto/crypto.dart' as crypto show md5;
+
+extension StringHelpers on String {
+  String get md5hex => crypto.md5.convert(utf8.encode(this)).toString();
+
   /// Truncate a string if it's longer than [maxLength] and add an [ellipsis].
   String truncate(int maxLength, [String ellipsis = '...']) =>
       length > maxLength
