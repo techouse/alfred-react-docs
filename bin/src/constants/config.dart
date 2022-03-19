872630e0910da2b6fab05bfab95bbc3b2334b7aa
Klemen Tušar <techouse@gmail.com>
2022-03-19 20:31:28 +0000

    :sparkles: Add automatic update

--- bin/src/constants/config.dart
+++ bin/src/constants/config.dart
@@ -1,6 +1,9 @@
 class Config {
   Config._();
 
+  static const String version = '2.0.3';
+  static final Uri githubRepositoryUrl =
+      Uri.https('github.com', '/techouse/alfred-react-docs');
   static const String algoliaApplicationId = 'BH4D9OD16A';
   static const String algoliaSearchOnlyApiKey =
       '36221914cce388c46d0420343e0bb32e';
