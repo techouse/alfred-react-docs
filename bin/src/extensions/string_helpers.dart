4bcb6e059b10c781a6ed08ef02675d2185b28430
Klemen Tusar <techouse@gmail.com>
2026-08-18 22:40:15 +0100

    build: replace Dart tooling with Rust packaging

--- bin/src/extensions/string_helpers.dart
+++
@@ -1,7 +0,0 @@
-extension StringHelpers on String {
-  /// Truncate a string if it's longer than [maxLength] and add an [ellipsis].
-  String truncate(int maxLength, [String ellipsis = '...']) =>
-      length > maxLength
-      ? '${substring(0, maxLength - ellipsis.length)}$ellipsis'
-      : this;
-}
